4df87114c6fd90fd6fe09e73df978fa0049e29e1
Andreas Krüger <Andreas.Krueger@innoQ.com>
2015-02-12 18:18:31 +0100

    Provide a fair random hidden number oracle.

--- base/src/main/scala/com/innoq/numbergame/base/OracleFactory.scala
+++ base/src/main/scala/com/innoq/numbergame/base/OracleFactory.scala
@@ -1,6 +1,7 @@
 package com.innoq.numbergame.base
 
 import com.innoq.numbergame.baseapi.Oracle
+import java.util.Random
 
 object OracleFactory {
 
@@ -9,4 +10,11 @@ object OracleFactory {
 
   def makeNiceOracle(length: Int, base: Int): Oracle =
     new NiceOracle(length, base)
+
+  def makeRandomFairOracle(length: Int, base: Int): Oracle = {
+    val r = new Random
+    val hiddenCode = new Array[Int](length)
+    (0 until length).foreach{hiddenCode(_) = r.nextInt(base)}
+    makeOracleFromHiddenCode(hiddenCode, base)
+  }
 }
---
+++ base_used_by_java/src/test/java/com/innoq/numbergame/base/RandomOracleTest.java
@@ -0,0 +1,37 @@
+package com.innoq.numbergame.base;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+import com.innoq.numbergame.baseapi.BadAttemptException;
+import com.innoq.numbergame.baseapi.Oracle;
+
+public class RandomOracleTest {
+	
+	@Test
+	public void test() throws BadAttemptException {
+		Oracle dut = OracleFactory.makeRandomFairOracle(3, 7);
+		assertEquals(Oracle.OracleType.FAIR, dut.getType());
+		assertEquals(3, dut.getLength());
+		assertEquals(7, dut.getBase());
+		
+		// Brute force search:
+		int[] attempt = new int[3];
+		boolean found = false;
+		allswell: for(int d0 = 0; d0 < 7; ++d0) {
+			attempt[0] = d0;
+			for(int d1 = 0; d1 < 7; ++d1) {
+				attempt[1] = d1;
+				for(int d2= 0; d2 < 7; ++d2) {
+					attempt[2] = d2;
+					if(dut.divinate(attempt).getFullMatchCount() == 2) {
+						found = true;
+						break allswell;
+					}
+				}
+			}
+		}
+		assertTrue(found);
+	}
+}
